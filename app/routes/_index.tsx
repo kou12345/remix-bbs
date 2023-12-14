150e22c4fe0e31f00169e6a0fa6ab691ea1c37bc
kou12345 <hi.0412.ko.2002@gmail.com>
2023-12-14 20:55:09 +0900

    Update index page with login form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,26 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { authenticator } from "~/services/auth.server";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      <Form method="post">
+        <input type="text" name="userName" required />
+        <input type="password" name="password" autoComplete="current-password" required />
+        <button>Sign In</button>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  return await authenticator.authenticate("user-login", request, {
+    successRedirect: "success",
+    failureRedirect: "/",
+  });
 }
