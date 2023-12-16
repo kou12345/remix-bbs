ebc91592198bb2fcb8721efc4d9cada053bee7e1
kou12345 <hi.0412.ko.2002@gmail.com>
2023-12-16 16:27:24 +0900

    Add server configuration for drizzle-orm
    This commit adds a new server configuration file for drizzle-orm. It imports the necessary dependencies, sets the required environment variables, and creates a client using the provided URL and authentication token. The created client is then exported for use in the application.

---
+++ app/drizzle/config.server.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+
+const url = process.env.TURSO_URL;
+if (!url) {
+  throw new Error("TURSO_URL is not set");
+}
+
+const authToken = process.env.TURSO_AUTH_TOKEN;
+if (!authToken) {
+  throw new Error("TURSO_AUTH_TOKEN is not set");
+}
+
+const client = createClient({ url, authToken });
+
+export const db = drizzle(client);
