85a717e2762e78fc36d03521fea43971bb4cb0f6
kou12345 <hi.0412.ko.2002@gmail.com>
2023-12-19 11:42:44 +0900

    Add login and signup functionality to auth server

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -2,15 +2,36 @@ import { Authenticator } from "remix-auth";
 import { FormStrategy } from "remix-auth-form";
 import { login } from "./login.server";
 import { sessionStorage } from "./session.server";
+import { signup } from "./signup.server";
 
 export const authenticator = new Authenticator<string>(sessionStorage);
 
+// ログイン
 authenticator.use(
   new FormStrategy(async ({ form }) => {
-    const userName = form.get("userName");
-    const password = form.get("password");
-    const userId = await login(String(userName), String(password));
-    return userId;
+    try {
+      const userName = form.get("userName");
+      const password = form.get("password");
+      const userId = await login(String(userName), String(password));
+      console.log("ログイン成功");
+      return String(userId);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }),
   "user-login"
+);
+
+// サインアップ
+authenticator.use(
+  new FormStrategy(async ({ form }) => {
+    const userName = form.get("userName") as string;
+    const password = form.get("password") as string;
+    console.log(userName, password);
+
+    const userId = await signup(userName, password);
+    return String(userId);
+  }),
+  "user-signup"
 );
