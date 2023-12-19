69f662b79b60e19fe255e04e97dc9c7d8ac039c1
kou12345 <hi.0412.ko.2002@gmail.com>
2023-12-19 11:42:33 +0900

    Add signup route and form

---
+++ app/routes/signup/index.tsx
@@ -0,0 +1,28 @@
+import { ActionFunctionArgs } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { db } from "~/drizzle/config.server";
+import { users } from "~/drizzle/schema";
+import { authenticator } from "~/services/auth.server";
+
+export default function Index() {
+  return (
+    <div>
+      <div>Sign Up</div>
+
+      <Form method="post">
+        <input type="text" name="userName" required />
+        <input type="password" name="password" autoComplete="current-password" required />
+        <button>Sign Up</button>
+      </Form>
+    </div>
+  );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const result = await db.select().from(users);
+  console.log(result);
+  return await authenticator.authenticate("user-signup", request, {
+    successRedirect: "success",
+    failureRedirect: "/",
+  });
+}
