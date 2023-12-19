5f0771d4208fb4f6afa28a59c25623fb40ea40b0
kou12345 <hi.0412.ko.2002@gmail.com>
2023-12-19 15:10:41 +0900

    Add user information to MyPageIndex component

--- app/routes/mypage/index.tsx
+++ app/routes/mypage/index.tsx
@@ -1,16 +1,44 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { eq } from "drizzle-orm";
+import { db } from "~/drizzle/config.server";
+import { users } from "~/drizzle/schema";
 import { authenticator } from "~/services/auth.server";
+import { getSession } from "~/services/session.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  return await authenticator.isAuthenticated(request, {
+  await authenticator.isAuthenticated(request, {
     failureRedirect: "/",
   });
+
+  // sessionを取得する
+  const session = await getSession(request.headers.get("cookie"));
+
+  // ログイン中のユーザー情報を取得する
+  try {
+    const result = await db
+      .select({
+        id: users.id,
+        name: users.name,
+      })
+      .from(users)
+      .where(eq(users.id, session.data.user));
+    console.log("result: ", result);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default function MyPageIndex() {
+  const user = useLoaderData<typeof loader>();
+  console.log(user);
   return (
     <div>
-      <div>マイページ</div>
+      <h2>マイページ</h2>
+      <div>ユーザー名: {user[0].name}</div>
+      <hr />
+      {/* いいねした投稿を表示する */}
     </div>
   );
 }
