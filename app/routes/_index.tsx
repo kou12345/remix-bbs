4717a405a9a88e709dc01c36133d3467436b052c
kou12345 <hi.0412.ko.2002@gmail.com>
2023-12-19 11:42:26 +0900

    Update routes file to include session loader and sign up link

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,29 @@
-import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import {
+  json,
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import { db } from "~/drizzle/config.server";
 import { users } from "~/drizzle/schema";
 import { authenticator } from "~/services/auth.server";
+import { getSession } from "~/services/session.server";
 
 export const meta: MetaFunction = () => {
   return [{ title: "New Remix App" }, { name: "description", content: "Welcome to Remix!" }];
 };
 
+export async function loader({ request }: LoaderFunctionArgs) {
+  const session = await getSession(request.headers.get("cookie"));
+  return json({ session });
+}
+
 export default function Index() {
+  // sessionを取得する
+  const session = useLoaderData<typeof loader>();
+  console.log(session.session.data.user);
+
   return (
     <div>
       <Form method="post">
@@ -16,6 +31,7 @@ export default function Index() {
         <input type="password" name="password" autoComplete="current-password" required />
         <button>Sign In</button>
       </Form>
+      <a href="/signup">Sign Up</a>
     </div>
   );
 }
